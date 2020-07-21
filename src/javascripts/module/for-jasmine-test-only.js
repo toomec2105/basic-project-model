fb3b64e774395b6af43eb2017c6ce4eda7ace8da
darco2018 <newsecond@outlook.com>
2020-07-21 21:06:17 +0200

    add README.md

--- src/javascripts/module/for-jasmine-test-only.js
+++ src/javascripts/module/for-jasmine-test-only.js
@@ -1,16 +1,16 @@
- // add this comment at the top to modify the given rule on this page  ---> /* eslint no-console: 0 */
+// add this comment at the top to modify the given rule on this page  ---> /* eslint no-console: 0 */
 
- // unncomment to test eslint CTRL SHIFT A
- /* console.log("Javascript connected");
+// unncomment to test eslint CTRL SHIFT A
+/* console.log("Javascript connected");
 const myvar = 1; */
 
-
 // -------------- test jasmine -----------------------
-const _ = require("lodash");
+const _ = require('lodash');
 
- function addTwoNumbersWithLodash(num1, num2) {
-        return _.add(num1, num2);
- }
+function addTwoNumbersWithLodash(num1, num2) {
+  return _.add(num1, num2);
+}
+
 module.exports = {
-    addTwoNumbersWithLodash
+  addTwoNumbersWithLodash,
 };
